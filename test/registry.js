08de5676277ef7979376ffe27d092a6fdbb6f058
boo1ean <boo1ean0807@gmail.com>
2014-01-18 14:28:41 +0200

    Add test for available migrations getter

--- test/registry.js
+++ test/registry.js
@@ -22,7 +22,7 @@ describe('Registry', function() {
 	};
 
 	env = {
-		path: './migrations',
+		path: __dirname + '/migrations',
 		storage: storage
 	};
 
@@ -42,5 +42,16 @@ describe('Registry', function() {
 		registry.putCompletedMigrations(val, spy);
 		spy.should.have.been.calledOnce;
 		spy.should.have.been.calledWith(JSON.stringify(val));
+	});
+
+	it('Should return list of available migrations', function() {
+		var expectedMigrations = [
+			'1390047616649-such-migration.js',
+			'1390047623625-many-steps.js',
+			'1390047653908-so-sequential.js'
+		];
+
+		var migrations = registry.getAvailableMigrations();
+		migrations.should.have.members(expectedMigrations);
 	});
 });
